8b7fa1d9e030a11e4be0ab8b7a6ada921f681f0e
benjamin <benjamin@gnbit.com>
2024-01-22 15:37:35 -0500

    initial commit

---
+++ docker-enter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -it alabra.entity /bin/bash
---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Alabra\Eloquent\Exception;
+
+/**
+ * Marker interface for package-specific exceptions.
+ */
+interface ExceptionInterface
+{
+
+}
